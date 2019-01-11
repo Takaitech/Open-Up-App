ee56676a14da33ba30d9cc114b9088644dfded09
Takaitech <takai.tech2144@gmail.com>
2019-01-11 11:02:43 -0800

    Travis-ci

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+
---
+++ tests/test-server.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+
+chai.use(chaiHttp);
+
